cc65f6256db65e9625fb240b095411942d8314e6
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-12 23:07:16 -0500

    [Add] Added cors and json middleware

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // implement your API here
 const express = require('express');
-
+const cors = require('cors');
 const server = express();
 const port  = 5000;
+
+server.use(express.json());
+server.use(cors());
+
+
+
 
 server.listen(port, () => console.log(`Server Runnin on port: ${port}`));
