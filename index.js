ffb301f999ffb846f40847a5fac15e4d829c2efd
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-12 23:00:35 -0500

    [Initial] Initial file, dependency and server setup

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+const port  = 5000;
+
+server.listen(port, () => console.log(`Server Runnin on port: ${port}`));
