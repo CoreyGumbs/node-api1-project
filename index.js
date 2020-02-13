a7aecec0d81d3cb9c2377aa8b9a3700680118516
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-12 23:31:08 -0500

    [Add] Added get to find all users to server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // implement your API here
 const express = require('express');
 const cors = require('cors');
+const db = require('./data/db.js');
+
 const server = express();
 const port  = 5000;
 
@@ -10,7 +12,18 @@ server.use(cors());
 
 server.get('/', (req, res) => {
     res.json({"hello": "world"});
-})
+});
+
+server.get('/api/users', (req, res)=> {
+    db.find()
+    .then( user => {
+        res.status(200).json(user);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({"errorMsg" : error});
+    });
+});
 
 
 server.listen(port, () => console.log(`Server Runnin on port: ${port}`));
