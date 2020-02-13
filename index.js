87f8155c6b3a01a8bd075d3087bf0f518a475239
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-13 01:37:35 -0500

    [Add] Added Conditional rendering to endpoints

--- index.js
+++ index.js
@@ -23,26 +23,30 @@ server.get('/api/users', (req, res)=> {
     })
     .catch(error => {
         console.log(error);
-        res.status(500).json({"errorMessage": "The users information could not be retrieved."});
+        res.status(500).json({error: "The users information could not be retrieved."});
     });
 });
 
 //GET LIST OF USER BY ID
-server.get('/api/users/:id', (req, res) => {
+server.get('/api/users/:id', (req, res, next) => {
     db.findById(req.params.id)
-    .then(user => {
-        res.status(200).json(user);
+    .then(user => { 
+        if(user.id !== req.params.id){
+        res.status(404).end().json({message: "The user with the specified ID does not exist." })
+        }else{
+            console.log(user);
+            res.status(200).json(user);
+        }
+        
     })
     .catch(error => {
-        console.log(error);
-        res.status(500).json({"errorMsg" : error});
+        res.status(500).json({ error: "The user information could not be retrieved." });
     });
 });
 
 //POST USER
 server.post('/api/users', (req, res) => {
-    const data = req.body;
-    console.log(req.body);
+
     if(req.body.name  === null || req.body.name  === ''){
 
         res.status(400).json({ "errorMessage": "Please provide name and bio for the user." });
@@ -52,14 +56,13 @@ server.post('/api/users', (req, res) => {
         res.status(400).json({ "errorMessage": "Please provide name and bio for the user." });
 
     }else{
-
-        db.insert(data)
+        db.insert(req.body)
         .then(user => {
             res.status(201).json(user)
         })
         .catch(error => {
             console.log(error);
-            res.status(500).json({"errorMsg" : error});
+            res.status(500).json({"errorMsg" : "There was an error while saving the user to the database"});
         });     
     };
 });
